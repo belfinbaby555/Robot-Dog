d5bcebf6d51c7cbc1a0404bd58e2f61f816afc27
Belfin Baby <105835749+belfinbaby555@users.noreply.github.com>
2024-10-22 19:42:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 function mshow() {
-    let event = window.event;
-    let x = event.clientX-450;
-    let y = event.clientY;
+   
+    let x = [100,0,-100,0,100];
+    let y = [340,340,340,250,340];
+    let w=0;
 
-    let dist = Math.sqrt((x) * (x) + y * y);
+    setInterval(()=>{
+
+        let dist = Math.sqrt((x[w]) * (x[w]) + y[w] * y[w]);
     
-    let t1 = Math.acos(x / dist);
+    let t1 = Math.acos(x[w] / dist);
     let theta1 = t1 * (180 / Math.PI);
     
     let argument = (dist / 2) / 224;
@@ -15,13 +18,36 @@ function mshow() {
     let theta = theta1 + theta2;
     let phi = 180 - (theta2 + 90);
     
-    document.getElementById('shoulder').style.transform = `rotate(${theta}deg)`;
-    document.getElementById('elbow').style.transform = `rotate(${phi*2}deg)`;
-    document.getElementById("point").style.margin = `${y}px ${x+450}px`;
-    document.getElementById("cords").innerHTML = `cords=(${x}, ${y}) φ=${Math.round(phi*2)}° θ=${Math.round(theta)}°`;
+    document.getElementById('shoulder1').style.transform = `rotate(${theta}deg)`;
+    document.getElementById('elbow1').style.transform = `rotate(${phi*2}deg)`;
+
+    setTimeout(()=>{
+        document.getElementById('shoulder2').style.transform = `rotate(${theta}deg)`;
+    document.getElementById('elbow2').style.transform = `rotate(${phi*2}deg)`;
+
+    document.getElementById('shoulder3').style.transform = `rotate(${theta}deg)`;
+    document.getElementById('elbow3').style.transform = `rotate(${phi*2}deg)`;
+
+    },1250)
+    
+
+    document.getElementById('shoulder4').style.transform = `rotate(${theta}deg)`;
+    document.getElementById('elbow4').style.transform = `rotate(${phi*2}deg)`;
+    
     
    
     console.log(theta1);
+
+    
+
+    if(w>=x.length-1){
+        w=0;
+    }
+    else{
+        w++;
+    }
+
+    },500)
 }
 
 
